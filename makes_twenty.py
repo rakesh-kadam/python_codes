e5965f7f20fb03bdef1c5185094b46e76912ca30
Hacker@135 <rakeshkadam26498@gmail.com>
2019-09-19 22:59:30 +0530

    intial commit

---
+++ makes_twenty.py
@@ -0,0 +1,3 @@
+#Given two integers, return True if the sum of the integers is 20 or if one of the integers is 20. If not, return False
+def makes_twenty(n1,n2):
+    return (n1+n2)==20 or n1==20 or n2==20
