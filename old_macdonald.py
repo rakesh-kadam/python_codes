e99df3f7853d7fd84b3dbd077a3862a34fcf5fec
Hacker@135 <rakeshkadam26498@gmail.com>
2019-09-19 23:07:14 +0530

    inital commit

---
+++ old_macdonald.py
@@ -0,0 +1,10 @@
+# Write a function that capitalizes the first and fourth letters of a name
+
+def old_macdonald(name):
+    if len(name) > 3:
+        return name[:3].capitalize() + name[3:].capitalize()
+    else:
+        return 'Name is too short!'
+
+# Check
+old_macdonald('macdonald')
