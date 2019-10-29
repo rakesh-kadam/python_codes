7524556de4d6afddc59be5790d60dd005d379b99
rakesh-kadam <rakeshkadam26498@gmail.com>
2019-10-29 23:35:07 +0530

    string

---
+++ udacity/string_playground.py
@@ -0,0 +1,7 @@
+# Browse the complete list of string methods at:
+# https://docs.python.org/3/library/stdtypes.html#string-methods
+# and try them out here
+
+print("my name is rakesh".capitalize())
+
+print("my name is rakesh".split(' ',2))
