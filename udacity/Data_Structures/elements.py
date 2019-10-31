a7038fce25e4db17d8b8ba5ff27cb13c60767720
Rakesh Kadam <rakeshkadam26498@gmail.com>
2019-10-31 19:51:23 +0530

    commit

---
+++ udacity/Data_Structures/elements.py
@@ -0,0 +1,10 @@
+elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H'},
+            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He'}}
+
+# todo: Add an 'is_noble_gas' entry to the hydrogen and helium dictionaries
+# hint: helium is a noble gas, hydrogen isn't
+
+elements['hydrogen']['is_nobal_gas'] = False
+elements['helium']['is_nobal_gas']= True
+
+print(elements)
